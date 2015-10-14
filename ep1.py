545866ff7aa6bae17d15f5ee996358e03d5ed632
kiko <kikoasgs@hotmail.com>
2015-10-14 20:20:20 -0300

    n chess python

--- ep1.py
+++ ep1.py
@@ -1,7 +1,42 @@
-from pyeda.boolalg.bdd import bddvar
+from pyeda.inter import *
 
-a, b = map(bddvar, 'ab')
-assert isinstance(b, object)
-assert isinstance(a, object)
-f = (a & ~b | ~a & b | ~a & ~b | a & b)
-print(f.is_one())
+X = exprvars('x', 8, 8)
+
+R = And(*[OneHot(*[X[r, c] for c in range(8)]) for r in range(8)])
+C = And(*[OneHot(*[X[r, c] for r in range(8)]) for c in range(8)])
+
+starts = [(i, 0) for i in range(8-2, 0, -1)] + [(0, i) for i in range(8-1)]
+lrdiags = []
+
+for r, c in starts:
+    lrdiags.append([])
+    ri, ci = r, c
+    while ri < 8 and ci < 8:
+        lrdiags[-1].append((ri, ci))
+        ri += 1
+        ci += 1
+
+DLR = And(*[OneHot0(*[X[r, c] for r, c in diag]) for diag in lrdiags])
+
+starts = [(i, 8-1) for i in range(8-2, -1, -1)] + [(0, i) for i in range(8-2, 0, -1)]
+rldiags = []
+
+for r, c in starts:
+    rldiags.append([])
+    ri, ci = r, c
+    while ri < 8 and ci >= 0:
+        rldiags[-1].append((ri, ci))
+        ri += 1
+        ci -= 1
+
+DRL = And(*[OneHot0(*[X[r, c] for r, c in diag]) for diag in rldiags])
+
+S = R & C & DLR & DRL
+
+print(S.is_cnf())
+print(len(S.xs))
+
+
+
+
+
