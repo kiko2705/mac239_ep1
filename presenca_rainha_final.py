103fd87efe87a5c233b6440d2b97567c545074d8
kiko <kikoasgs@hotmail.com>
2015-10-28 04:32:35 -0200

    gera-clausulas iniciado 1

--- presenca_rainha_final.py
+++ presenca_rainha_final.py
@@ -2,43 +2,35 @@ __author__ = 'kiko'
 
 # INSERE DADO INICIAL
 N = int(input('Entre com o tamanho do tabuleiro : '))
-tamanho_lista_clausulas = N*N
 
 # cláusulas para a presença de uma rainha em cada linha
 # r11 ∨ r12 (C1)
 # r21 ∨ r22 (C2)
 # etc...
 
-# índice linha matriz tabuleiro
-num_linhas = N
-# índice coluna matriz tabuleiro
-num_colunas = N
-
-# define matriz cláusula ( C1, C2, etc... )
-c = [0 for x in range(N*N)]
-# define matriz temp cláusula ( C1, C2, etc... )
-c_temp = [0 for x in range(N*N)]
-
-indice_clausula = 0
 linha  = 0
 coluna = 0
 
-# define a matriz de rainhas bidimensional, que representa cada rainha
-r = [[0 for x in range(N)] for x in range(N)]
+numero_clausulas = 0
+
+# string cada casa tabuleiro (r11, r12, etc..)
+nome_casa = "r"
+
+# define dicionario que conterá cláusulas presenca rainhas
+# do tipo dic = { "r11 V r12": C1, "r21 V r22": C2}
+dicionario_clausulas_presenca = {}
 
 # loop para preencher dicionario presença rainha
 # número de cláusulas a serem criadas será NxN
 for linha in range(N):
     for coluna in range(N):
-        r[linha][coluna] = int(input())
-        print('r', linha+1, coluna+1, '=', r[linha][coluna])
-        c_temp[indice_clausula] = r[linha][coluna]
-        c[indice_clausula] = c[indice_clausula] | c_temp[indice_clausula]
-        print('C', indice_clausula+1, '=', c[indice_clausula])
-    indice_clausula = indice_clausula + 1
+        # coloca todos os componentes da primeira cláusula ( " r11 V r12 V etc... )
+        nome_casa = nome_casa + str(linha+1) + str(coluna+1) + " V "
+        print(nome_casa)
+        nome_casa = "r"
+    print("(C", numero_clausulas+1, ")")
+    numero_clausulas = numero_clausulas + 1
 
-# define dicionario que conterá cláusulas presenca rainhas
-# do tipo dic = { "r11 V r12": C1, "r21 V r22": C2}
-dicionario_clausulas_presenca = {}
+
 
 
