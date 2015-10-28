90e724f8544310557c633f88603bd6081c04d687
kiko <kikoasgs@hotmail.com>
2015-10-28 06:11:28 -0200

    gera-clausulas iniciado 1

--- restricao_diagonais_final.py
+++ restricao_diagonais_final.py
@@ -3,9 +3,9 @@ __author__ = 'kiko'
 # INSERE DADO INICIAL
 N = int(input('Entre com o tamanho do tabuleiro : '))
 
-# cláusulas para a presença de uma rainha em cada linha
-# r11 ∨ r12 (C1)
-# r21 ∨ r22 (C2)
+# cláusulas de restrição a não ataques nas diagonais
+# ¬r 11 ∨ ¬r 22
+# ¬r 12 ∨ ¬r 21
 # etc...
 
 linha  = 0
@@ -26,15 +26,10 @@ for linha in range(N):
         contador_numero_casas = contador_numero_casas + 1
         if coluna == 0:
             cnf = cnf + "("
-        nome_casa = "r" + str(linha+1) + str(coluna+1)
+        nome_casa = "~r" + str(linha+1) + str(coluna+1)
         cnf = cnf + nome_casa
         if coluna != (N-1):
             cnf = cnf + " | "
     cnf = cnf + ")"
     cnf = cnf + " & "
 print(cnf)
-
-
-
-
-
